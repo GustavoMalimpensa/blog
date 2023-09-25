2088800630dc6ed27daac226d09dd7a2bc4ad701
Gustavo <gumalimpensa@gmail.com>
2023-09-25 10:14:38 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './pages/footer/footer.component';
-import { NavegacaoComponent } from './pages/navegacao/navegacao.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavegacaoComponent } from './shared/navegacao/navegacao.component';
 import { ContatoComponent } from './pages/contato/contato.component';
 import { ExpComponent } from './pages/exp/exp.component';
 import { InicioComponent } from './pages/inicio/inicio.component'
